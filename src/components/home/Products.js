b060d2e1616b4d2fde68082914f0a7395ff360ea
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2024-07-17 18:00:59 +0400

    new

--- src/components/home/Products.js
+++ src/components/home/Products.js
@@ -1,14 +1,13 @@
-// components/Products.jsx
-
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./products.module.css";
-import { FaHeart  } from "react-icons/fa";
-import { CgRepeat } from "react-icons/cg";
-import { FaCartShopping  } from "react-icons/fa6";
-
+import { FaHeart } from "react-icons/fa";
+import { MdRepeat } from "react-icons/md";
+import { MdShoppingCart } from "react-icons/md";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [sortOption, setSortOption] = useState("");
 
   useEffect(() => {
     fetchProducts();
@@ -16,33 +15,95 @@ const Products = () => {
 
   const fetchProducts = async () => {
     try {
-      const response = await fetch('https://backend-faceless-dec3a8930688.herokuapp.com/api/data');
+      const response = await fetch(
+        "https://backend-faceless-dec3a8930688.herokuapp.com/api/data"
+      );
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
+
       const data = await response.json();
       setProducts(data);
     } catch (error) {
-      console.error('Error fetching data:', error);
-      // Handle error state if needed
+      console.log("Error Fetching Data: ", error);
     }
+  };
+
+  const handleSort = (e) => {
+    setSortOption(e.target.value);
+  };
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  const sertedAndFilteredProducts = () => {
+    let sortedProducts = [...products];
+
+    if (selectedCategory) {
+      sortedProducts = sortedProducts.filter(
+        (product) => product.category === selectedCategory
+      );
+    }
+    switch (sortOption) {
+      case "name":
+        sortedProducts.sort((a, b) => a.name.localeCompare(b.name));
+        break;
+      case "priceAsc":
+        sortedProducts.sort(
+          (a, b) => parseFloat(a.price) - parseFloat(b.price)
+        );
+        break;
+      case "priceDesc":
+        sortedProducts.sort(
+          (a, b) => parseFloat(b.price) - parseFloat(a.price)
+        );
+        break;
+      default:
+        break;
+    }
+
+    return sortedProducts;
   };
 
   return (
     <div className={styles.container}>
+      <div className={styles.controls}>
+        <select onChange={handleSort} value={sortOption}>
+          <option value="">Sort By</option>
+          <option value="name">name</option>
+          <option value="priceAsc">Price: Low To High</option>
+          <option value="priceDesc">Price: High To Low</option>
+        </select>
+
+        <select onChange={handleCategoryChange} value={selectedCategory}>
+          <option value="">All Category</option>
+          <option value="ხილი">ხილი</option>
+          <option value="ხილ-ბოსტნეული">ხილ-ბოსტნეული</option>
+        </select>
+      </div>
+
       <div className={styles.list}>
-        {products.map((product) => (
-          <div key={product._id} className={styles.item}>
+        {sertedAndFilteredProducts().map((product) => (
+          <div className={styles.item} key={product._id}>
             <div className={styles.imager}>
-              <img src={`https://backend-faceless-dec3a8930688.herokuapp.com${product.photo}`} alt="product" />
+              <img
+                src={`https://backend-faceless-dec3a8930688.herokuapp.com${product.photo}`}
+                alt={product.name}
+              />
               <div className={styles.additional}>
-                <div><FaHeart /></div>
-                <div><CgRepeat /></div>
-                <div><FaCartShopping /></div>
+                <div>
+                  <FaHeart />
+                </div>
+                <div>
+                  <MdRepeat />
+                </div>
+                <div>
+                  <MdShoppingCart />
+                </div>
               </div>
             </div>
             <div className={styles.name}>{product.name}</div>
-            <div className={styles.price}>${product.price}</div>
+            <div className={styles.price}>{product.price}</div>
           </div>
         ))}
       </div>
